8894eb1ff7e5fcfa1ab13a87a101175b6ba7e08e
Dan Chen <dchen@taipeifx.com>
2018-07-02 00:07:33 +0800

    adding draw.py

---
+++ draw.py
@@ -0,0 +1,23 @@
+import turtle
+
+painter = turtle.Turtle()
+
+for _ in range(3):
+	painter.forward(100)
+	painter.left(90)
+	painter.forward(100)
+	painter.left(90)
+	painter.forward(100)
+	painter.left(90)
+	painter.forward(100)
+
+	painter.right(90)
+	painter.forward(100)
+	painter.right(90)
+	painter.forward(100)
+	painter.right(90)
+	painter.forward(100)
+	painter.right(90)
+	painter.forward(100)
+
+turtle.done()
